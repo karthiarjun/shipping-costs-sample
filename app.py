ecc9a79ff52c95f05cdfb2488f76a07b9ceee022
karthiarjun <arjunkarthik1991@gmail.com>
2017-09-19 00:33:30 +0530

    Res

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def makeWebhookResult(req):
     zone = parameters.get("shipping-zone")
     #cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
     r = requests.get('http://services.odata.org/Northwind/Northwind.svc/Customers?$format=json&$filter=substringof%28%27Alfreds%27,%20CompanyName%29%20eq%20true')
-    data = json.loads(resp.text)
+    data = json.loads(r.text)
     speech = data.odata.metadata
     print("Response:")
     print(speech)
